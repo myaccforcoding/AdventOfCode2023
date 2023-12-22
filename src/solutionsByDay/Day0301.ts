42d9ea8eec97ead736b2f092e7719b349ef9103d
myaccforcoding <37026497+myaccforcoding@users.noreply.github.com>
2023-12-22 16:28:48 +0100

    day 03 refactoring

--- src/solutionsByDay/Day0301.ts
+++ src/solutionsByDay/Day0301.ts
@@ -2,58 +2,85 @@ import { readLocalFile } from "../helper/localFileReader";
 
 export async function runSolution(): Promise<number> {
     const lines = await readLocalFile("src\\inputFiles\\Day03.txt");
-    const regex = /\d+/g;
+    const numberRegex = /\d+/g;
     let totalSum = 0;
 
     lines.forEach((line, index) => {
-        let number;
+        let matchedNumberInLine;
         const y = index;
 
-        while ((number = regex.exec(line)) !== null) {
-            const neighbourCharacters = [];
+        while ((matchedNumberInLine = numberRegex.exec(line)) !== null) {
+            const charsAdjacentToCurrentNumber = [];
 
+            // checks if a line above the current one exists and gets all chars adjacent to the top of the current number
             if (lines[y - 1] !== undefined && lines[y - 1] !== "") {
-                const start = Math.max(0, number.index - 1);
-                const end = Math.min(
-                    number.index + number[0].length + 1,
+                const startColumnIndex = Math.max(
+                    0,
+                    matchedNumberInLine.index - 1,
+                );
+                const endColumnIndex = Math.min(
+                    matchedNumberInLine.index +
+                        matchedNumberInLine[0].length +
+                        1,
                     lines[y - 1].length,
                 );
 
-                for (let x = start; x < end; x++) {
-                    neighbourCharacters.push(lines[y - 1][x]);
+                for (let x = startColumnIndex; x < endColumnIndex; x++) {
+                    charsAdjacentToCurrentNumber.push(lines[y - 1][x]);
                 }
             }
 
+            // checks if a column to the left of the current one exists and gets the char adjacent to the left of the current number
             if (
-                line[number.index - 1] !== undefined &&
-                line[number.index - 1] !== ""
+                line[matchedNumberInLine.index - 1] !== undefined &&
+                line[matchedNumberInLine.index - 1] !== ""
             ) {
-                neighbourCharacters.push(line[number.index - 1]);
+                charsAdjacentToCurrentNumber.push(
+                    line[matchedNumberInLine.index - 1],
+                );
             }
 
+            // checks if a column to the right of the current one exists and gets the char adjacent to the right of the current number
             if (
-                line[number.index + number[0].length] !== undefined &&
-                line[number.index + number[0].length] !== ""
+                line[
+                    matchedNumberInLine.index + matchedNumberInLine[0].length
+                ] !== undefined &&
+                line[
+                    matchedNumberInLine.index + matchedNumberInLine[0].length
+                ] !== ""
             ) {
-                neighbourCharacters.push(line[number.index + number[0].length]);
+                charsAdjacentToCurrentNumber.push(
+                    line[
+                        matchedNumberInLine.index +
+                            matchedNumberInLine[0].length
+                    ],
+                );
             }
 
+            // checks if a line below the current one exists and gets all chars adjacent to the below of the current number
             if (lines[y + 1] !== undefined && lines[y + 1] !== "") {
-                const start = Math.max(0, number.index - 1);
+                const startColumnIndex = Math.max(
+                    0,
+                    matchedNumberInLine.index - 1,
+                );
                 const end = Math.min(
-                    number.index + number[0].length + 1,
+                    matchedNumberInLine.index +
+                        matchedNumberInLine[0].length +
+                        1,
                     lines[y + 1].length,
                 );
 
-                for (let x = start; x < end; x++) {
-                    neighbourCharacters.push(lines[y + 1][x]);
+                for (let x = startColumnIndex; x < end; x++) {
+                    charsAdjacentToCurrentNumber.push(lines[y + 1][x]);
                 }
             }
-            const hasSpecialChar = neighbourCharacters.some(
+
+            const hasSpecialChar = charsAdjacentToCurrentNumber.some(
                 (char) => char !== "." && char !== undefined,
             );
+
             if (hasSpecialChar) {
-                totalSum += parseInt(number[0]);
+                totalSum += parseInt(matchedNumberInLine[0]);
             }
         }
     });
--- src/solutionsByDay/Day0302.ts
+++ src/solutionsByDay/Day0302.ts
@@ -10,135 +10,73 @@ interface StarsAndAdjacentNumbers {
 
 export async function runSolution(): Promise<number> {
     const lines = await readLocalFile("src\\inputFiles\\Day03.txt");
-    const regex = /\d+/g;
+    const numberRegex = /\d+/g;
     let totalSum = 0;
-    const allStartsAndNeighborNumbers: StarsAndAdjacentNumbers[] = [];
+    const allStarsAndAdjacentNumbers: StarsAndAdjacentNumbers[] = [];
 
-    lines.forEach((line, index) => {
+    function processAdjacentNumbers(
+        x: number,
+        y: number,
+        adjacentNumber: number,
+    ) {
+        const existingEntryIndex = allStarsAndAdjacentNumbers.findIndex(
+            (starEntry) =>
+                starEntry.starCoordinates.x === x &&
+                starEntry.starCoordinates.y === y,
+        );
+
+        if (existingEntryIndex >= 0) {
+            allStarsAndAdjacentNumbers[existingEntryIndex].adjacentNumbers.push(
+                adjacentNumber,
+            );
+        } else {
+            allStarsAndAdjacentNumbers.push({
+                adjacentNumbers: [adjacentNumber],
+                starCoordinates: { x, y },
+            });
+        }
+    }
+
+    lines.forEach((line, y) => {
         let number;
-        const y = index;
+        while ((number = numberRegex.exec(line)) !== null) {
+            const currentNumber = parseInt(number[0]); // Convert to number
+            const startX = Math.max(0, number.index - 1);
+            const endX = Math.min(
+                number.index + number[0].length + 1,
+                line.length,
+            );
 
-        while ((number = regex.exec(line)) !== null) {
-            if (lines[y - 1] !== undefined && lines[y - 1] !== "") {
-                const start = Math.max(0, number.index - 1);
-                const end = Math.min(
-                    number.index + number[0].length + 1,
-                    lines[y - 1].length,
-                );
-
-                for (let x = start; x < end; x++) {
-                    const character = lines[y - 1][x];
-                    if (character === "*") {
-                        const existingEntryIndex =
-                            allStartsAndNeighborNumbers.findIndex(
-                                (starEntry) =>
-                                    starEntry.starCoordinates.x === x &&
-                                    starEntry.starCoordinates.y === y - 1,
-                            );
-                        if (existingEntryIndex >= 0) {
-                            allStartsAndNeighborNumbers[
-                                existingEntryIndex
-                            ].adjacentNumbers.push(parseInt(number[0]));
-                        } else {
-                            allStartsAndNeighborNumbers.push({
-                                adjacentNumbers: [parseInt(number[0])],
-                                starCoordinates: { x, y: y - 1 },
-                            });
-                        }
-                    }
+            for (let x = startX; x < endX; x++) {
+                if (y > 0 && lines[y - 1][x] === "*") {
+                    processAdjacentNumbers(x, y - 1, currentNumber);
                 }
-            }
 
-            if (
-                line[number.index - 1] !== undefined &&
-                line[number.index - 1] !== ""
-            ) {
-                const x = number.index - 1;
-                const character = lines[y][x];
-                if (character === "*") {
-                    const existingEntryIndex =
-                        allStartsAndNeighborNumbers.findIndex(
-                            (starEntry) =>
-                                starEntry.starCoordinates.x === x &&
-                                starEntry.starCoordinates.y === y,
-                        );
-                    if (existingEntryIndex >= 0) {
-                        allStartsAndNeighborNumbers[
-                            existingEntryIndex
-                        ].adjacentNumbers.push(parseInt(number[0]));
-                    } else {
-                        allStartsAndNeighborNumbers.push({
-                            adjacentNumbers: [parseInt(number[0])],
-                            starCoordinates: { x, y },
-                        });
-                    }
+                if (lines[y][x] === "*" && x !== number.index - 1) {
+                    processAdjacentNumbers(x, y, currentNumber);
                 }
-            }
 
-            if (
-                line[number.index + number[0].length] !== undefined &&
-                line[number.index + number[0].length] !== ""
-            ) {
-                const x = number.index + number[0].length;
-                const character = lines[y][x];
-                if (character === "*") {
-                    const existingEntryIndex =
-                        allStartsAndNeighborNumbers.findIndex(
-                            (starEntry) =>
-                                starEntry.starCoordinates.x === x &&
-                                starEntry.starCoordinates.y === y,
-                        );
-                    if (existingEntryIndex >= 0) {
-                        allStartsAndNeighborNumbers[
-                            existingEntryIndex
-                        ].adjacentNumbers.push(parseInt(number[0]));
-                    } else {
-                        allStartsAndNeighborNumbers.push({
-                            adjacentNumbers: [parseInt(number[0])],
-                            starCoordinates: { x, y },
-                        });
-                    }
+                if (
+                    lines[y][x] === "*" &&
+                    x !== number.index + number[0].length
+                ) {
+                    processAdjacentNumbers(x, y, currentNumber);
                 }
-            }
 
-            if (lines[y + 1] !== undefined && lines[y + 1] !== "") {
-                const start = Math.max(0, number.index - 1);
-                const end = Math.min(
-                    number.index + number[0].length + 1,
-                    lines[y + 1].length,
-                );
-
-                for (let x = start; x < end; x++) {
-                    const character = lines[y + 1][x];
-                    if (character === "*") {
-                        const existingEntryIndex =
-                            allStartsAndNeighborNumbers.findIndex(
-                                (starEntry) =>
-                                    starEntry.starCoordinates.x === x &&
-                                    starEntry.starCoordinates.y === y + 1,
-                            );
-                        if (existingEntryIndex >= 0) {
-                            allStartsAndNeighborNumbers[
-                                existingEntryIndex
-                            ].adjacentNumbers.push(parseInt(number[0]));
-                        } else {
-                            allStartsAndNeighborNumbers.push({
-                                adjacentNumbers: [parseInt(number[0])],
-                                starCoordinates: { x, y: y + 1 },
-                            });
-                        }
-                    }
+                if (y < lines.length - 1 && lines[y + 1][x] === "*") {
+                    processAdjacentNumbers(x, y + 1, currentNumber);
                 }
             }
         }
     });
 
-    const gears = allStartsAndNeighborNumbers.filter(
+    const gears = allStarsAndAdjacentNumbers.filter(
         (star) => star.adjacentNumbers.length === 2,
     );
 
     gears.forEach((gear) => {
         totalSum += gear.adjacentNumbers[0] * gear.adjacentNumbers[1];
     });
+
     return totalSum;
 }
